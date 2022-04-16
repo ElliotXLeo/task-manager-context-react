e18dc57d3a314f64eb0c3a740f2bababa0c95556
ElliotXLeo <elliotgaramendi@gmail.com>
2022-04-16 14:17:41 -0500

    component TasksList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from 'react-router-dom';
 import Header from './components/sections/Header';
-import './App.css';
+import TasksList from './components/tasks/TasksList';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/new-task" element={<h1>New Task</h1>} />
-        <Route index element={<h1>Task Manager Context React</h1>} />
+        <Route index element={<TasksList />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
     </Router>
---
+++ src/components/tasks/TasksList.jsx
@@ -0,0 +1,7 @@
+const TasksList = () => {
+  return (
+    <h1>Task Manager Context React</h1>
+  );
+}
+
+export default TasksList;
